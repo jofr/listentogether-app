5acb66bdd3f8a31cc538004c258a9612a79a2c4e
jofr <11932850-jfromgallifrey@users.noreply.gitlab.com>
2022-07-08 18:56:48 +0200

    Add plugin to handle Media Session API and foreground service (for background playback) on Android

--- android/app/src/main/java/io/gitlab/listentogether/MainActivity.java
+++ android/app/src/main/java/io/gitlab/listentogether/MainActivity.java
@@ -1,7 +1,13 @@
 package io.gitlab.listentogether;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.webkit.ServiceWorkerClient;
+import android.webkit.ServiceWorkerController;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebResourceResponse;
+
 import com.getcapacitor.BridgeActivity;
 import com.getcapacitor.PluginHandle;
 
@@ -10,7 +16,19 @@ public class MainActivity extends BridgeActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         registerPlugin(ShareTargetPlugin.class);
+
+        if(Build.VERSION.SDK_INT >= 24 ){
+            ServiceWorkerController swController = ServiceWorkerController.getInstance();
+
+            swController.setServiceWorkerClient(new ServiceWorkerClient() {
+                @Override
+                public WebResourceResponse shouldInterceptRequest(WebResourceRequest request) {
+                    return bridge.getLocalServer().shouldInterceptRequest(request);
+                }
+            });
+        }
     }
 
     @Override
@@ -19,7 +37,6 @@ public class MainActivity extends BridgeActivity {
 
         String action = intent.getAction();
         String type = intent.getType();
-
         if (Intent.ACTION_SEND.equals(action)) {
             String text = intent.getStringExtra(Intent.EXTRA_TEXT);
             PluginHandle handle = bridge.getPlugin("ShareTarget");
