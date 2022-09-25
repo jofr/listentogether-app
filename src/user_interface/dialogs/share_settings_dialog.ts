462f30e90d0d92afef590ce74a1c6de931fa7db8
jofr <11932850-jfromgallifrey@users.noreply.gitlab.com>
2022-09-25 23:46:23 +0200

    feat: possibility to leave/end listening, m3 fixes

--- src/user_interface/dialogs/share_settings_dialog.ts
+++ src/user_interface/dialogs/share_settings_dialog.ts
@@ -3,6 +3,8 @@ import { customElement } from "lit/decorators";
 
 import { ModalDialog } from "./modal_dialog";
 import { SessionController } from "../controllers/session";
+import { ListeningHost } from "../../listening/peer";
+import { ListeningSession } from "../../listening/session";
 
 declare global {
     interface HTMLElementTagNameMap {
@@ -15,13 +17,36 @@ export class ShareSettingsDialog extends ModalDialog {
     static styles = [
         ModalDialog.styles,
         css`
+            mwc-button {
+                --mdc-theme-primary: var(--error);
+                --mdc-theme-on-primary: var(--on-error);
+            }
         `
     ];
 
     private sessionController = new SessionController(this);
 
+    private endListening() {
+        window.session = ListeningSession.CreateHost();
+        this.hide();
+    }
+
+    private leaveListening() {
+        window.session = ListeningSession.CreateHost();
+        this.hide();
+    }
+
     renderContent() {
-        return html`   
-        `;
+        const session = this.sessionController.session;
+
+        if (session?.peer && session.peer instanceof ListeningHost) {
+            return html`
+                <mwc-button unelevated icon="exit_to_app" label="End listening" @click=${this.endListening}></mwc-button>
+            `;
+        } else {
+            return html`
+                <mwc-button unelevated icon="logout" label="Leave listening" @click=${this.leaveListening}></mwc-button>
+            `;
+        }        
     }
 }
