a66da0939915606249f892e4f2059e99adad18fd
jofr <11932850-jfromgallifrey@users.noreply.gitlab.com>
2022-09-26 00:34:45 +0200

    refactor: readability

--- src/player/audio_element_player.ts
+++ src/player/audio_element_player.ts
@@ -1,76 +1,80 @@
 import { Events } from "../util/events";
 import { AudioPlayer } from "./audio_player";
-import { SyncedListeningState } from "../listening/sync";
+import { SyncedListeningState } from "../listening/synced_state";
 import { logger } from "../util/logger";
 
 export class AudioElementPlayer extends Events implements AudioPlayer {
-    private _listeningState: SyncedListeningState;
+    private state: SyncedListeningState = null;
     private audioElement: HTMLAudioElement;
+    private silenceSrc: string = `${window.location.origin}${window.location.pathname}silence.mp3`;
 
     constructor() {
         super();
 
-        this.audioElement = document.createElement("audio");
-        this.audioElement.src = `${window.location.origin}${window.location.pathname}silence.mp3`;
+        this.audioElement = new Audio(this.silenceSrc);
         document.body.addEventListener("pointerdown", this.silentActivation);
 
-        this.audioElement.addEventListener("timeupdate", () => this.emit("timeupdate"));
-        this.audioElement.addEventListener("pause", () => this.emit("pause"));
-        this.audioElement.addEventListener("play", () => this.emit("play"));
-        this.audioElement.addEventListener("durationchange", () => this.emit("durationchange"));
-        this.audioElement.addEventListener("seeked", () => this.emit("seeked"));
-        this.audioElement.addEventListener("ended", () => this.emit("ended"));
+        for (const event of ["timeupdate", "pause", "play", "durationchange", "seeked", "ended"]) {
+            this.audioElement.addEventListener(event, () => this.emit(event));
+        }
     }
 
     private silentActivation = () => {
         this.audioElement.play().then(() => {
-            if (this._listeningState.playback.paused) {
+            if (this.state == null || this.state.playback.paused) {
                 this.audioElement.pause();
             }
             document.body.removeEventListener("pointerdown", this.silentActivation);
-            logger.log("AudioElementPlayer silently activated");
+            logger.debug("AudioElementPlayer silently activated");
         }).catch(error => {
-            logger.log("Error while trying to silently activate AudioElemenPlayer:", error);
+            logger.warn("Error while trying to silently activate AudioElemenPlayer:", error);
         });
     }
 
-    private changeAudio = (): void => {
-        this.audioElement.src = this._listeningState.playback.currentAudio;
-        if (!this._listeningState.playback.paused) {
-            this.audioElement.play();
+    private updateAudio = (): void => {
+        const newSrc = this.state?.playback.currentAudio;
+        if (this.audioElement.src !== newSrc && newSrc !== null) {
+            this.audioElement.src = newSrc;
+            if (!this.state.playback.paused) {
+                this.audioElement.play();
+            }
+            this.emit("audiochange");
         }
-        this.emit("audiochange");
     }
 
-    private updatePlay = (): void => {
-        if (this._listeningState.playback.paused && !this.audioElement.paused) {
+    private updatePlayPause = (): void => {
+        if (this.state?.playback.paused && !this.audioElement.paused) {
             this.audioElement.pause();
-        } else if (!this._listeningState.playback.paused && this.audioElement.paused) {
+        } else if (this.state && !this.state.playback.paused && this.audioElement.paused) {
             this.audioElement.play();
         }
     }
 
-    private changeTime = (): void => {
-        if (this._listeningState.playback.paused) {
-            this.audioElement.currentTime = this._listeningState.playback.audioTime;
-        } else {
+    private updateTime = (): void => {
+        if (this.state?.playback.paused) {
+            this.audioElement.currentTime = this.state.playback.audioTime;
+        } else if (this.state) {
             const now = Date.now();
-            const diff = (now - this._listeningState.playback.referenceTime) / 1000.0;
-            this.audioElement.currentTime = this._listeningState.playback.audioTime + diff;
+            const diff = (now - this.state.playback.referenceTime) / 1000.0;
+            this.audioElement.currentTime = this.state.playback.audioTime + diff;
         }
     }
 
     set listeningState(state: SyncedListeningState) {
-        /* TODO: Should unsubscribe from possible previous listeningState (there might be more than one synchronized state) */
+        if (this.state !== null) {
+            this.state.unsubscribe(["playback/currentAudio"], this.updateAudio);
+            this.state.unsubscribe(["playback/paused"], this.updatePlayPause)
+            this.state.unsubscribe(["playback/referenceTime", "playback/audioTime"], this.updateTime);
+        }
 
-        this._listeningState = state;
-        this._listeningState.subscribe(["playback/currentAudio"], this.changeAudio);
-        this._listeningState.subscribe(["playback/paused"], this.updatePlay)
-        this._listeningState.subscribe(["playback/referenceTime", "playback/audioTime"], this.changeTime);
+        this.state = state;
+        this.state.subscribe(["playback/currentAudio"], this.updateAudio);
+        this.state.subscribe(["playback/paused"], this.updatePlayPause)
+        this.state.subscribe(["playback/referenceTime", "playback/audioTime"], this.updateTime);
 
-        this.changeAudio();
-        this.updatePlay();
-        this.changeTime();
+        this.updateAudio();
+        this.updatePlayPause();
+        this.updateTime();
     }
 
     get duration(): number {
